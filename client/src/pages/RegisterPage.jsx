bb27ac6ce5880d0f4b080bbad0fc116f4cce1dbf
Abhinav <abhinavag1504@gmail.com>
2024-06-01 00:16:50 +0530

    first commit

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function RegisterPage() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    function registerUser(ev){
+        ev.preventDefault();
+            // axios.post('/register', {
+            //     name,
+            //     email,
+            //     password,
+            // })
+            axios.get('https://localhost:4000/test')
+    }
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="-mt-64">
+                <h1 className="text-4xl text-center mb-4">Register</h1>
+                <form className="max-w-md mx-auto " onSubmit={registerUser}>
+                    <input type="text" placeholder="Tony Stack" value={name} onChange={ev => setName(ev.target.value)} />
+                    <input type="email" placeholder="your@email.com"  value={email} onChange={ev => setEmail(ev.target.value)}/>
+                    <input type="password" placeholder="Password" value={password} onChange={ev => setPassword(ev.target.value)}/>
+                    <button className="primary" type="submit">Register</button>
+                    <div className="text-center py-2 text-gray-500">
+                        Already a member? <Link className="underline text-black" to={"/login"}>Login</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
